6712b973f042a5a51b712c1403bc637c79891aab
Ryan McClure <mcc@mcc3d.com>
2024-02-28 17:47:16 -0500

    Further divided up the shader fragment into logical chunks.
    * Removed ugly polynomial trendline equation from Black mode, replacing
      it with the same curve that White mode used.
    * Split up more of the fragment shader into discrete static properties,
      which can be applied as-needed. Now, each shader just has a unique
      "what value do I use?" function (luma or inverse luma), and the rest
      of the code is shared.

--- modules/backgroundShader.js
+++ modules/backgroundShader.js
@@ -15,16 +15,36 @@ export class InfravisionColorationVisionShaderBase extends ColorationVisionShade
         return coeff * exp(-1. * (xOffset * xOffset) / (2. * sigma * sigma));
     }`;
 
-    // This is rather-obviously derived from a trendline
-    // TODO: Replace this with some version of the tanh() used for white mode
-    static BLUE_CURVE_FUNCTION = `
-    float blueCurve(in float x) {
-        return 2.86778398510243 * x * x * x - 4.32322426177177 * x * x + 2.44143389199256 * x - 0.00242418196328738;
-    }`;
-
     static HYPERBOLIC_TANGENT_FUNCTION = `
     float tanh(in float x) {
         return (exp(x) - exp(-1.*x)) / (exp(x) + exp(-1.*x));
+    }`;
+
+    static COMPUTE_LUMA_FUNCTION = `
+    float computeLuminance(in vec3 baseColor) {
+        return dot(vec3(0.30, 0.59, 0.11), baseColor.rgb);
+    }`;
+
+    static INFRAVISION_BACKGROUND_FUNCTION = `
+    vec3 infravision(in float x) {
+        return vec3(
+            0.,
+            0.00390625 * exp(5.54517744447956 * x),
+            tanh((x - 0.5) * 4. ) / 2.169 + (41./72.)
+        );
+    }`;
+
+    static MAIN_INFRAVISION_FRAGMENT = `
+    void main(void) {
+        ${this.FRAGMENT_BEGIN}
+        float x = getInputValue(baseColor.rgb);
+        if (baseColor.a > alphaThreshold) {
+            gl_FragColor.rgb = infravision(x);
+            gl_FragColor.a = baseColor.a;
+        }
+        else {
+            gl_FragColor = vec4(0.0);
+        }
     }`;
 
     static fragmentShader = "";
@@ -47,22 +67,15 @@ export class InfravisionColorationVisionShaderBlack extends InfravisionColoratio
     ${this.PERCEIVED_BRIGHTNESS}
     uniform float alphaThreshold;
 
-    ${this.GAUSSIAN_CURVE_FUNCTION}
-    ${this.BLUE_CURVE_FUNCTION}
+    ${this.HYPERBOLIC_TANGENT_FUNCTION}
+    ${this.COMPUTE_LUMA_FUNCTION}
+    ${this.INFRAVISION_BACKGROUND_FUNCTION}
 
-    void main(void) {
-        ${this.FRAGMENT_BEGIN}
-        float luminance = dot(vec3(0.30, 0.59, 0.11), baseColor.rgb);
-        if (baseColor.a > alphaThreshold) {
-            gl_FragColor.r = 0.;
-            gl_FragColor.g = gaussianCurve(luminance, 0.5, 0.15, 0.5);
-            gl_FragColor.b = blueCurve(luminance); // gaussianCurve(luminance, 0.9, 0.325, 1.021) - 0.022; // -1.*luminance*luminance + 2.*luminance;
-            gl_FragColor.a = baseColor.a;
-        }
-        else {
-            gl_FragColor = vec4(0.0);
-        }
-    }`;
+    float getInputValue(in vec3 baseColor) {
+        return computeLuminance(baseColor);
+    }
+
+    ${this.MAIN_INFRAVISION_FRAGMENT}`;
 }
 
 /**
@@ -75,18 +88,12 @@ export class InfravisionColorationVisionShaderWhite extends InfravisionColoratio
     uniform float alphaThreshold;
 
     ${this.HYPERBOLIC_TANGENT_FUNCTION}
-    void main(void) {
-        ${this.FRAGMENT_BEGIN}
-        float luminance = dot(vec3(0.30, 0.59, 0.11), baseColor.rgb);
-        float invLum = 1. - luminance;
-        if (baseColor.a > alphaThreshold) {
-            gl_FragColor.r = 0.;
-            gl_FragColor.g = 0.00390625 * exp(5.54517744447956 * invLum);
-            gl_FragColor.b = tanh((invLum - 0.5) * 4.) / 2.169 + (41./72.);
-            gl_FragColor.a = baseColor.a;
-        }
-        else {
-            gl_FragColor = vec4(0.0);
-        }
-    }`;
+    ${this.COMPUTE_LUMA_FUNCTION}
+    ${this.INFRAVISION_BACKGROUND_FUNCTION}
+
+    float getInputValue(in vec3 baseColor) {
+        return 1. - computeLuminance(baseColor);
+    }
+
+    ${this.MAIN_INFRAVISION_FRAGMENT}`;
 }
